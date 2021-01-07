7f704aef2853dd3d6357fa665df2db8b22c7cd6a
davidcloak <35105111+davidcloak@users.noreply.github.com>
2021-01-07 14:56:49 -0500

    Debug Now False

--- webSite/settings.py
+++ webSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e3=&3%4z_g7-8zwsmf3bb@)-z@&-harx^_)l$gi86v!#l+)*-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #edit
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] #edit
 
 
 # Application definition
