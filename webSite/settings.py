55e01b5b7233961442bdad4649a591156406cbdf
davidcloak <35105111+davidcloak@users.noreply.github.com>
2021-01-07 14:49:59 -0500

    Add Azure deployment files

--- webSite/settings.py
+++ webSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e3=&3%4z_g7-8zwsmf3bb@)-z@&-harx^_)l$gi86v!#l+)*-$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
