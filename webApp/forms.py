856af79667cbca924ef91274bd44e1eebb355d0a
davidcloak <35105111+davidcloak@users.noreply.github.com>
2021-01-19 00:01:59 -0500

    whyyyy

--- webApp/forms.py
+++ webApp/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from webApp.models import User
 
-class UserForm(forms.ModelForm):
+class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['fName', 'lName', 'email', 'password']
